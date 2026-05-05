b509a689607b84313f4da39ddf61eaeed2593aca
Engineer Fred <engfred88@gmail.com>
2026-05-05 10:40:14 +0300

    updated

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,21 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('database', () => ({
-  host: process.env.DB_HOST ?? 'localhost',
-  port: parseInt(process.env.DB_PORT ?? '5432', 10),
-  username: process.env.DB_USERNAME ?? 'postgres',
-  password: process.env.DB_PASSWORD ?? 'postgres',
-  name: process.env.DB_NAME ?? 'rentfinda_db',
-}));
+export default registerAs('database', () => {
+  if (process.env.DATABASE_URL) {
+    const url = new URL(process.env.DATABASE_URL);
+    return {
+      host: url.hostname,
+      port: parseInt(url.port, 10),
+      username: url.username,
+      password: url.password,
+      name: url.pathname.replace('/', ''),
+    };
+  }
+  return {
+    host: process.env.DB_HOST ?? 'localhost',
+    port: parseInt(process.env.DB_PORT ?? '5432', 10),
+    username: process.env.DB_USERNAME ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? 'postgres',
+    name: process.env.DB_NAME ?? 'rentfinda_db',
+  };
+});
